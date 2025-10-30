92f4d61c82baa354390a1b9e36f6889a883f1c22
Bwe942 <71046264+Bwe942@users.noreply.github.com>
2025-10-30 21:36:07 +0000

    cavalo adicionado

--- xadrez.c
+++ xadrez.c
@@ -2,13 +2,15 @@
 
 int main (){
     //variaveis utilizadas no futuro
-    int bispo =0 , rainha = 0;
+    int bispo =0 , rainha = 0 , cavalo =0;
 
     //torre movendo 5 casas para direita
     for (int i = 0; i < 5; i++)
     {
         printf("Torre moveu-se para: Direita\n"); //imprimindo a movimentaçao da peça
     }
+
+    printf("\n"); // separando as peças
 
     //bispo movendo na diagonal
     do
@@ -17,11 +19,27 @@ int main (){
        bispo++;
     } while (bispo < 5 );
 
+    printf("\n"); // separando as peças
+    
     //rainha movendo para esquerda
     while (rainha <8)
+
     {
        printf("Rainha moveu-se para: Esquerda\n");//imprimindo a movimentaçao da peça
        rainha++;
+    }
+
+    printf("\n"); // separando as peças
+
+    //cavalo movimentando
+    for (int j = 0; j <1 ; j++)
+    {
+       while (cavalo < 2)
+       {
+        printf("Cavalo Moveu-se Para: Baixo\n"); // imprime baixo 2 vezes
+        cavalo++;
+       }
+       printf("Cavalo Moveu-se Para: Esquerda\n"); // imprime esquerda no final do loop anterior
     }
     
 return 0;
